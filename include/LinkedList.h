42308ea6fffc7076d9e68634bc23a84c9a419a41
squarep n_6g <ashida6@gmail.com>
2014-09-15 10:08:42 +0900

    [ADD] OOP系統のための準備

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -1,13 +1,22 @@
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
 typedef void *Data;
 
-typedef struct _linkedlist *LinkedList;
+typedef struct _node{
+  int index;
+  Data data;
+  void *next;
+  void *prev;
+} Node;
+typedef struct __linkedlist *LinkedList;
+typedef void (*fptrEachFunc)(Node*);
 
 extern LinkedList linkedlist_initialize();
-extern Data linkedlist_get_from_index(LinkedList, int);
 extern Data linkedlist_get_next(LinkedList);
 extern void linkedlist_free(LinkedList);
-extern void linkedlist_set_index(LinkedList, int, Data);
-extern void linkedlist_set(LinkedList, Data);
-
+extern void linkedlist_add(LinkedList, Data);
+extern void linkedlist_move_pos(LinkedList, int);
+extern void linkedlist_each_function(LinkedList, fptrEachFunc);
 extern int linkedlist_errno;
 
+#endif
--- include/nlang.h
+++ include/nlang.h
@@ -1,13 +1,14 @@
 #ifndef __NLANG_H__
-#define __NLANG_H__
+#  define __NLANG_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <getopt.h>
+#  include <stdio.h>
+#  include <string.h>
+#  include <stdlib.h>
+#  include <stdbool.h>
+#  include <unistd.h>
+#  include <getopt.h>
 
-#include "LinkedList.h"
+#  include "LinkedList.h"
 
 /* lex.c */
 void lex_from_file(const char*);
@@ -15,4 +16,64 @@ void lex_from_file(const char*);
 /* parser.c */
 void parse_from_file(const char*);
 
+typedef enum {
+  T_OPERATOR, 
+  T_KEYWORD, 
+  T_LITERAL, 
+} TokenKind;
+
+typedef enum {
+  ACC_PUBLIC,
+  ACC_PROTECTED,
+  ACC_PRIVATE
+} AccessModifiers;
+
+typedef enum {
+  M_METHOD,
+  M_FIELD
+} MemberKind;
+
+typedef struct _field {
+  Data value;
+} Field;
+
+typedef struct _method {
+  LinkedList bytecode;
+} Method;
+
+typedef struct _classmember {
+  char *name;
+  AccessModifiers am;
+  MemberKind type;
+  union {
+    Field f;
+    Method m;
+  } u;
+} ClassMember;
+
+typedef struct _class {
+  char* fqdn;
+  AccessModifiers am;
+  ClassMember **static_members;
+  ClassMember **members;
+} Class;
+
+typedef struct _object {
+  char* unique_name;
+  Class class; 
+} Object;
+
+#  define INIT_PTR(type, var) type *var = NULL;
+#  define free_s(ptr) do { if(ptr != NULL) {free(ptr);} } while(0);
+#  define malloc_s(ptr, type, size) do { ptr = (type *) malloc(sizeof(type) * size); memset(ptr, '\0', sizeof(type) * size); } while(0);
+#    ifndef NDEBUG
+#      define debug_log(message, values...) do{ \
+    fprintf(stderr, "FILE: %s\n", __FILE__); \
+    fprintf(stderr, "LINE: %d\n", __LINE__); \
+    fprintf(stderr, "FUNCTION: %s\n", __FUNCTION__); \
+    fprintf(stderr, message, values); \
+  } while(0);
+#    else
+#      define debug_log(x) 
+#    endif
 #endif //__NLANG_H__
